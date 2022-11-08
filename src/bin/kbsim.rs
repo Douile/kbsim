2de771da7041defe063f590b9c97561ba7701b2f
Douile <25043847+Douile@users.noreply.github.com>
2022-11-08 16:19:16 +0000

    Fallback to reading from stdin when no text provided

--- src/bin/kbsim.rs
+++ src/bin/kbsim.rs
@@ -1,7 +1,7 @@
 use structopt::StructOpt;
 
-use std::fs;
-use std::io::{Error, ErrorKind, Result};
+use std::fs::{self, OpenOptions};
+use std::io::{Error, ErrorKind, Read, Write};
 use std::thread;
 use std::time::Duration;
 
@@ -48,7 +48,7 @@ struct CliOpt {
     string: Option<String>,
 }
 
-fn main() -> Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let CliOpt {
         hid_file,
         layout,
@@ -65,9 +65,8 @@ fn main() -> Result<()> {
         return Ok(());
     }
 
+    let hid_file = hid_file.unwrap_or_else(|| "/dev/hidg0".to_string());
     if let Some(mut string) = string {
-        let hid_file = hid_file.unwrap_or_else(|| "/dev/hidg0".to_string());
-
         if newline {
             string.push('\n');
         }
@@ -81,6 +80,40 @@ fn main() -> Result<()> {
             fs::write(&hid_file, packet)?;
             thread::sleep(Duration::from_millis(cooldown));
         }
+    } else {
+        eprintln!("Reading from stdin");
+
+        let mut hid_file = OpenOptions::new().write(true).open(hid_file)?;
+
+        let mut term = terminal::stdout();
+        term.act(terminal::Action::EnableRawMode)?;
+
+        let mut stdin = std::io::stdin();
+        let mut buf = [0u8; 128];
+        while let Ok(n) = stdin.read(&mut buf[..]) {
+            if n == 0 {
+                continue;
+            }
+            if buf.contains(&3) {
+                // Break on ctrl+c
+                break;
+            }
+            match std::str::from_utf8(&buf[..n]) {
+                Ok(text) => {
+                    term.write(&buf[..n])?;
+                    term.flush()?;
+                    let hid_bytes = keyboard_layouts::string_to_hid_packets(&layout, text)
+                        .map_err(|e| Error::new(ErrorKind::Other, format!("{}", e)))?;
+
+                    for packet in hid_bytes.chunks(keyboard_layouts::HID_PACKET_LEN) {
+                        hid_file.write(packet)?;
+                        hid_file.flush()?;
+                    }
+                }
+                Err(e) => eprintln!("Could not decode character {} {:?}", e, &buf),
+            };
+        }
+        term.act(terminal::Action::DisableRawMode)?;
     }
 
     Ok(())
